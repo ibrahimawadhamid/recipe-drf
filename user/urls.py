8eab951856464646c86c379bcf6137ff4eda27c0
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2021-05-30 19:59:20 +0200

    Added manage user endpoint

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import CreateUserView, CreateTokenView
+from .views import CreateUserView, CreateTokenView, ManageUserView
 from .apps import UserConfig
 
 app_name = UserConfig.name
@@ -8,4 +8,5 @@ app_name = UserConfig.name
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name="create"),
     path('token/', CreateTokenView.as_view(), name="token"),
+    path('me/', ManageUserView.as_view(), name="me"),
 ]
