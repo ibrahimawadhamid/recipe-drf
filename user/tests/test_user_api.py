f41bf703915f576cc58b85e48947b80ede672220
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2021-05-30 14:36:08 +0200

    Adjust travis run command

--- user/tests/test_user_api.py
+++ user/tests/test_user_api.py
@@ -1,5 +1,32 @@
-# from django.test import TestCase
-# from django.contrib.auth import get_user_model
-# from django.urls import reverse
-# from rest_framework.test import APIClient
-# from rest_framework import status
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
+
+CREATE_USER_URL = reverse("user:create")
+
+
+def create_user(**kwargs):
+    return get_user_model().objects.create_user(**kwargs)
+
+
+class PublicUserApiTest(TestCase):
+    """Test the users API (public)"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """Test creating user with a valid payload is successful"""
+        payload = {
+            'email': 'test@domain.com',
+            'password': "Password123",
+            'name': 'John Doe'
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
